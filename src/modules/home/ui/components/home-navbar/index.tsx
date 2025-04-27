beace03b0e07369f12077c9256b3d548b98aa935
HiroNaka1211 <njim1211@gmail.com>
2025-04-27 23:09:14 +0900

    Update nav bar design

--- src/modules/home/ui/components/home-navbar/index.tsx
+++ src/modules/home/ui/components/home-navbar/index.tsx
@@ -3,26 +3,35 @@ import Image from "next/image";
 import Link from "next/link";
 import { SearchInput } from "./search-input";
 import { AuthButton } from "@/modules/auth/ui/components/auth-button";
+import { robotoCondensed } from "@/app/layout";
 
 export const HomeNavbar = () => {
   return (
-    <nav className="fixed top-0 left-0 right-0 h-16 bg-white flex items-center px-2 pr-5 z-50">
+    <nav className="fixed top-0 left-0 right-0 h-[56px] bg-white flex items-center px-2  pr-5 z-50">
       <div className="flex items-center gap-4 w-full">
-        {/* {Menu and logo} */}
+        {/* Sidebar and logo */}
         <div className="flex items-center flex-shrink-0">
           <SidebarTrigger />
           <Link href="/">
-            <div className="p-4 flex items-center gap-1">
+            <div className="p-4 pr-0 flex items-center">
               <Image src="logo.svg" alt="logo" width={32} height={32} />
-              <p className="text-xl font-semibold tracking-tight">CloneTube</p>
+              <p
+                className={`${robotoCondensed.className} inline-block transform scale-y-125 text-xl font-semibold tracking-[-1px] relative`}
+              >
+                CloneTube
+                <span className="text-[10px] absolute -top-2 -right-3.5 transform-none tracking-normal">
+                  JP
+                </span>
+              </p>
             </div>
           </Link>
         </div>
+
         {/* search bar */}
-        <div className="flex-1 justify-center max-w-[720px] mx-auto">
+        <div className="flex-1  max-w-[720px] mx-auto">
           <SearchInput />
         </div>
-        <div className="flex-shrink-0 items-center flex gap-4">
+        <div className="flex-shrink-0 flex gap-4">
           <AuthButton />
         </div>
       </div>
