02aca1e0d30612e83f0258894984edbf963ed46f
HiroNaka1211 <njim1211@gmail.com>
2025-06-29 17:17:56 +0900

    Update sentence on the main page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,8 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <Image src="/logo.svg" height={50} width={50} alt="logo" />
-      <p className="text-xl font-semibold tracking-tight">CloneTube</p>
+      <h1>Videos are coming soon...</h1>
     </div>
   );
 }
