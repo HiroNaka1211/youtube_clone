988a436e2f5a0c97d504b95e619aa82c769a30f9
HiroNaka1211 <njim1211@gmail.com>
2025-07-12 21:51:45 +0900

    Add sign-in and sign-out handling to main page

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,9 +4,8 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      {children}
-    </div>
+    // <div className="min-h-screen flex items-center justify-center">
+    <div>{children}</div>
   );
 };
 
